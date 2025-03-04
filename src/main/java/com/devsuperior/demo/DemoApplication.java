7b078c7ca8d2c19f4c881ed5df672e1938a6072f
myjobwell <70344092+myjobwell@users.noreply.github.com>
2025-03-03 19:20:26 -0500

    Add BCript

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+//public class DemoApplication implements CommandLineRunner {
+
+	public class DemoApplication {
+
+//	@Autowired
+//	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
+//	@Override
+//	public void run(String... args) throws Exception {
+//		System.out.println(passwordEncoder.encode("123456"));
+//
+//		boolean result = passwordEncoder.matches("123456", "$2a$10$Rp92DWslHAgtV7l/3m9Ave4kk2owMs0cNgszvOE/4l7ZfOVUnllbO");
+//
+//		System.out.println(result);
+//	}
+
 
 }
